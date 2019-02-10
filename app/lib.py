4c93a50539bc710788eb5df0a287b2467d9df271
Farida <1>
2019-02-10 10:29:56 +0300

    Added pytest

--- app/lib.py
+++ app/lib.py
@@ -1,15 +1,16 @@
+# coding=utf-8
 def is_creditable(age, salary):
     # первый тест позитивный
     """
-    >>>is_creditable(30,40_000)
+    >>> is_creditable(30,40_000)
     True
 
-    >>>is_creditable(20, 40_000)
+    >>> is_creditable(20, 40_000)
     False
 
-    >>>is_creditable(70, 40_000)
+    >>> is_creditable(70, 40_000)
     False
-    >>>is (30, 20_000)
+    >>> is_creditable (30, 20_000)
     False
     """
     min_age = 21
