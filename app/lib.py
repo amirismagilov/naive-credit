0d7658e27ab18c3e0df8d6cd6784ff2a1a4ddc81
Farida <1>
2019-02-10 11:04:07 +0300

    Coverage added

--- app/lib.py
+++ app/lib.py
@@ -2,15 +2,7 @@
 def is_creditable(age, salary):
     # первый тест позитивный
     """
-    >>> is_creditable(30,40_000)
-    True
-
     >>> is_creditable(20, 40_000)
-    False
-
-    >>> is_creditable(70, 40_000)
-    False
-    >>> is_creditable (30, 20_000)
     False
     """
     min_age = 21
